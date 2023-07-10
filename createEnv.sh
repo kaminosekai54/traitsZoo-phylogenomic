24c17f6e2f620810a274418c4b352f13d8c5c22a
kaminosekai54 <culpin.alexis@laposte.net>
2023-07-10 15:52:38 +0200

    adding create env

---
+++ createEnv.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --chdir=/home/genouest/cea/aculpin/
+#SBATCH --job-name=createEnv
+#SBATCH --output=create_env.o
+#SBATCH --error=create_env.e
+
+#langages
+. /softs/local/env/envpython-3.9.5.sh
+
+#tools
+. /local/env/envconda.sh
+
+
+conda env create -f environment.yml
+
+conda activate pipelinePhylo
+
+
+
