83a72dced383ef4e8b6c16217e49ca0a22849725
kaminosekai54 <culpin.alexis@laposte.net>
2023-07-12 12:11:58 +0200

    updating launch, removing useless import

--- launch.sh
+++ launch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=pipPhylo
+#SBATCH --job-name=raxmlTest
 #SBATCH --chdir=/scratch/aculpin/traitsZoo-phylogenomic/
-#SBATCH --output=pipeline_log.o
-#SBATCH --error=pipeline_log.e
+#SBATCH --output=raxmlTest_log.o
+#SBATCH --error=raxmlTest_log.e
 #SBATCH --mail-user=alexis.culpin@cri-paris.org
 #SBATCH --mail-type=none
 
@@ -11,19 +11,14 @@
 . /softs/local/env/envpython-3.9.5.sh
 
 #tools
-. /softs/local/env/envblast-2.9.0.sh
 . /softs/local/env/envdiamond-2.0.2.sh
 . /softs/local/env/envmafft-7.475.sh
-. /softs/local/env/envraxml-8.2.12.sh
-. /local/env/envconda.sh
-
 
 conda activate ~/pipeline
-
+. /softs/local/env/envraxml-8.2.12.sh
 
 python3 fastaRenamer.py
 python3 getAndFilterBlast.py
 python3 getAndFilterRBH.py
 python3 getAlignment.py
 python3 getTree.py
-
